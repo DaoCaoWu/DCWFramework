67fd0a8575b40f88bf514c971842c37e1937cdbd
adao12 <adao12@qq.com>
2015-08-05 00:16:40 +0800

    add welcome page

---
+++ Rating/app/src/main/java/com/dcw/app/rating/biz/welcome/WelcomeFragment.java
@@ -0,0 +1,44 @@
+package com.dcw.app.rating.biz.welcome;
+
+import android.os.Handler;
+
+import com.dcw.app.rating.R;
+import com.dcw.app.rating.biz.MainActivity;
+import com.dcw.app.rating.biz.home.HomeFragment;
+import com.dcw.app.rating.ui.adapter.BaseActivityWrapper;
+import com.dcw.app.rating.ui.adapter.BaseFragmentWrapper;
+import com.dcw.framework.view.annotation.InjectLayout;
+
+/**
+ * Created by adao12 on 2015/8/4.
+ */
+@InjectLayout(R.layout.fragment_welcome)
+public class WelcomeFragment extends BaseFragmentWrapper {
+
+    @Override
+    public Class getHostActivity() {
+        return MainActivity.class;
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initUI() {
+
+    }
+
+    @Override
+    public void initListeners() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getActivity().getSupportFragmentManager().popBackStack();
+                startFragment(HomeFragment.class);
+//                getActivity().getSupportFragmentManager().beginTransaction().remove(WelcomeFragment.this).commitAllowingStateLoss();
+            }
+        }, 1000);
+    }
+}
--- Rating/app/src/main/java/com/dcw/app/rating/ui/adapter/LauncherController.java
+++ Rating/app/src/main/java/com/dcw/app/rating/ui/adapter/LauncherController.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.dcw.app.rating.biz.home.HomeFragment;
 import com.dcw.app.rating.biz.test.RichTextFragment;
+import com.dcw.app.rating.biz.welcome.WelcomeFragment;
 import com.dcw.framework.pac.basic.BaseController;
 import com.dcw.framework.pac.basic.FrameworkMessage;
 import com.dcw.framework.pac.basic.IResultListener;
@@ -18,7 +19,7 @@ public class LauncherController extends BaseController {
     @Override
     public void handleMessage(String messageId, Bundle messageData, IResultListener listener) {
         if (messageId == FrameworkMessage.LAUNCHER_CONTROLLER_INVOKE) {
-            startFragment(HomeFragment.class);
+            startFragment(WelcomeFragment.class);
         }
     }
 }
