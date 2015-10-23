af01a77db1329c97fdc9cefcec1b363db12ec899
adao12 <adao12@qq.com>
2015-10-24 00:22:14 +0800

    add architecture

---
+++ Rating/app/src/main/java/com/dcw/app/rating/log/LogUtils.java
@@ -0,0 +1,71 @@
+package com.dcw.app.rating.log;
+
+
+import com.dcw.app.rating.BuildConfig;
+
+/**
+ * Author:    ZhuWenWu
+ * Version    V1.0
+ * Date:      14-11-22 14:09
+ * Description:Log 控制工具类
+ * Modification  History:
+ * Date         	Author        		Version        	Description
+ * -----------------------------------------------------------------------------------
+ * 14-11-22      ZhuWenWu            1.0                    1.0
+ * Why & What is modified:
+ */
+public class LogUtils {
+    public static void v(String tag, String msg, Throwable tr) {
+        if (BuildConfig.DEBUG) {
+            android.util.Log.v(tag, msg, tr);
+        }
+    }
+
+    public static void d(String tag, String msg) {
+        if (BuildConfig.DEBUG) {
+            android.util.Log.d(tag, msg);
+        }
+    }
+
+    public static void d(String tag, String msg, Throwable tr) {
+        if (BuildConfig.DEBUG) {
+            android.util.Log.d(tag, msg, tr);
+        }
+    }
+
+    public static void i(String tag, String msg) {
+        if (BuildConfig.DEBUG) {
+            android.util.Log.i(tag, msg);
+        }
+    }
+
+    public static void i(String tag, String msg, Throwable tr) {
+        if (BuildConfig.DEBUG) {
+            android.util.Log.i(tag, msg, tr);
+        }
+    }
+
+    public static void w(String tag, String msg, Throwable tr) {
+        if (BuildConfig.DEBUG) {
+            android.util.Log.w(tag, msg, tr);
+        }
+    }
+
+    public static void w(String tag, Throwable tr) {
+        if (BuildConfig.DEBUG) {
+            android.util.Log.w(tag, tr);
+        }
+    }
+
+    public static void e(String tag, String msg, Throwable tr) {
+        if (BuildConfig.DEBUG) {
+            android.util.Log.e(tag, msg, tr);
+        }
+    }
+
+    public static void e(String tag, String msg) {
+        if (BuildConfig.DEBUG) {
+            android.util.Log.e(tag, msg);
+        }
+    }
+}
---
+++ Rating/app/src/main/java/com/dcw/app/rating/network/callback/HttpBaseCallBack.java
@@ -0,0 +1,34 @@
+package com.dcw.app.rating.network.callback;
+
+
+import com.dcw.app.rating.log.LogUtils;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+/**
+ * Author:    ZhuWenWu
+ * Version    V1.0
+ * Date:      14-11-22 14:55
+ * Description: Http 数据请求 CallBack
+ * Modification  History:
+ * Date         	Author        		Version        	Description
+ * -----------------------------------------------------------------------------------
+ * 14-11-22      ZhuWenWu            1.0                    1.0
+ * Why & What is modified:
+ */
+public class HttpBaseCallBack<T> implements Callback<T> {
+    protected String TAG = HttpBaseCallBack.class.getSimpleName();
+
+    @Override
+    public void success(T data, Response response) {
+        LogUtils.d(TAG, "success--> url = " + response.getUrl());
+    }
+
+    @Override
+    public void failure(RetrofitError retrofitError) {
+        LogUtils.e(TAG, "failure--> url = " + retrofitError.getUrl());
+        retrofitError.printStackTrace();
+    }
+}
