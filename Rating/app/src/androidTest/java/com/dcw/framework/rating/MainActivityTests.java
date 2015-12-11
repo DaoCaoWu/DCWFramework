9121409fee02d4dd12925fda1c4b79e089145170
adao12 <adao12.vip@gmail.com>
2015-12-11 21:04:52 +0800

    add test

---
+++ Rating/app/src/androidTest/java/com/dcw/framework/rating/MainActivityTests.java
@@ -0,0 +1,46 @@
+package com.dcw.framework.rating;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+import com.dcw.app.R;
+import com.dcw.app.presentation.MainActivity;
+
+/**
+ * create by adao12.vip@gmail.com on 15/12/11
+ *
+ * @author JiaYing.Cheng
+ * @version 1.0
+ */
+public class MainActivityTests extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTests() {
+        super(MainActivity.class);
+    }
+
+    public void testActivityExists() {
+        MainActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+
+    public void testGreet() {
+        MainActivity activity = getActivity();
+
+        // Type name in text input
+        // ----------------------
+
+        final EditText nameEditText =
+                (EditText) activity.findViewById(R.id.et_account);
+
+        // Send string input value
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                nameEditText.requestFocus();
+            }
+        });
+
+        getInstrumentation().waitForIdleSync();
+        getInstrumentation().sendStringSync("13570320927");
+    }
+}
