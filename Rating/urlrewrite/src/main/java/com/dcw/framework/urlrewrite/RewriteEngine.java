993c409d3b46b43648a1a887081474ef51604007
adao12 <adao12.vip@gmail.com>
2015-12-12 19:07:25 +0800

    update

---
+++ Rating/urlrewrite/src/main/java/com/dcw/framework/urlrewrite/RewriteEngine.java
@@ -0,0 +1,13 @@
+package com.dcw.framework.urlrewrite;
+
+/**
+ * create by adao12.vip@gmail.com on 15/12/12
+ *
+ * @author JiaYing.Cheng
+ * @version 1.0
+ */
+public class RewriteEngine {
+
+    public RewriteEngine() {
+    }
+}
---
+++ Rating/urlrewrite/src/main/java/com/dcw/framework/urlrewrite/Rule.java
@@ -0,0 +1,10 @@
+package com.dcw.framework.urlrewrite;
+
+/**
+ * create by adao12.vip@gmail.com on 15/12/12
+ *
+ * @author JiaYing.Cheng
+ * @version 1.0
+ */
+public class Rule {
+}
